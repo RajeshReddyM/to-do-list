534a8f0046bd80d173004781a2fb7319eaafbee1
rajesh <rajesh@deeproot.in>
2015-10-01 23:28:44 -0400

    implement search using filter instead of query

--- app/todolist/scripts/IndexController.js
+++ app/todolist/scripts/IndexController.js
@@ -3,7 +3,6 @@ angular
   .controller("IndexController", function ($scope, Todolist, supersonic) {
     $scope.todolists = null;
     $scope.showSpinner = true;
-    $scope.searchtext = null;
 
     Todolist.all().whenChanged( function (todolists) {
         $scope.$apply( function () {
@@ -11,16 +10,6 @@ angular
           $scope.showSpinner = false;
         });
     });
-
-    $scope.search = function(searchtext) {
-      var query = {"text": searchtext}
-      Todolist.findAll({query: JSON.stringify(query)}).then( function (todolists) {
-        $scope.$apply(function() {
-          $scope.todolists = todolists;
-          $scope.showSpinner = false;
-        });
-      });
-    }
 
     $scope.remove = function (id) {
       $scope.showSpinner = true;
